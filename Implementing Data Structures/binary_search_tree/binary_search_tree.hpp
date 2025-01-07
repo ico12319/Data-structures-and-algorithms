a06bfa61637352d0839278d650ec031151f5a193
ico12319 <hristokubev@gmail.com>
2025-01-07 15:00:47 +0200

    add full bst implementation

--- Implementing Data Structures/binary_search_tree/binary_search_tree.hpp
+++ Implementing Data Structures/binary_search_tree/binary_search_tree.hpp
@@ -9,20 +9,33 @@ private:
         value val;
         node* left;
         node* right;
+        size_t subtree_size = 1;
         
-        node(const value& val) : val(val),left(nullptr),right(nullptr){}
-        node(const value& val,node* left,node* right) : val(val),left(left),right(right){}
+        node(const value& val) : val(val),left(nullptr),right(nullptr),subtree_size(1){}
     };
     
     node* root = nullptr;
     comparator cmp;
-    size_t size = 0;
     
     node* copy(node* root);
-    void print_helper(node* root) const;
-    node** find_min_node(node** root);
-    void move(const binary_search_tree& other);
-    void destroy(node* root);
+    void move(binary_search_tree&& other);
+    void destory(node* root);
+    node** find_min(node** root);
+    static void print_helper(node* root){
+        if(!root) return;
+        print_helper(root->left);
+        std::cout<<root->val<<" ";
+        print_helper(root->right);
+    }
+    static value& get_element_at_index(node* root,size_t index){
+        size_t current_index = root->left ? root->left->subtree_size : 0;
+        if(current_index > index)
+            get_element_at_index(root->left, index);
+        else if(current_index == index)
+            return root->val;
+        
+        return get_element_at_index(root->right, index - current_index - 1);
+    }
     
     class forward_iterator{
     private:
@@ -34,13 +47,12 @@ private:
                 root = root->left;
             }
         }
+        
     public:
         forward_iterator(node* root){
             push_left(root);
         }
-        value& operator*() const{
-            return nodes.top()->val;
-        }
+        
         forward_iterator& operator++(){
             node* curr_top = nodes.top();
             nodes.pop();
@@ -48,14 +60,21 @@ private:
                 push_left(curr_top->right);
             return *this;
         }
-        forward_iterator operator++(int){
+        
+        forward_iterator operator++(int  dummy){
             forward_iterator copy(*this);
             ++(*this);
             return copy;
         }
+        
+        const value& operator*() const{
+            return nodes.top()->val;
+        }
+        
         bool operator==(const forward_iterator& other) const{
             return nodes == other.nodes;
         }
+        
         bool operator!=(const forward_iterator& other) const{
             return !(*this == other);
         }
@@ -76,6 +95,7 @@ private:
         backward_iterator(node* root){
             push_right(root);
         }
+        
         backward_iterator& operator--(){
             node* curr_top = nodes.top();
             nodes.pop();
@@ -83,14 +103,17 @@ private:
                 push_right(curr_top->left);
             return *this;
         }
-        backward_iterator operator--(int){
+        
+        backward_iterator operator--(int dummy){
             backward_iterator copy(*this);
             --(*this);
             return copy;
         }
-        value& operator*() const{
+        
+        const value& operator*() const{
             return nodes.top()->val;
         }
+        
         bool operator==(const backward_iterator& other) const{
             return nodes == other.nodes;
         }
@@ -101,12 +124,19 @@ private:
     
 public:
     binary_search_tree() = default;
-    binary_search_tree(const comparator& cmp);
     binary_search_tree(const binary_search_tree& other);
     binary_search_tree(binary_search_tree&& other);
     binary_search_tree& operator=(const binary_search_tree& other);
     binary_search_tree& operator=(binary_search_tree&& other);
     ~binary_search_tree();
+    
+    void insert(const value& _value);
+    void remove(const value& _value);
+    bool contains(const value& _value) const;
+    const value& operator[](size_t index) const;
+    size_t size() const;
+    bool is_empty() const;
+    
     forward_iterator forward_begin() const{
         return forward_iterator(root);
     }
@@ -116,117 +146,48 @@ public:
     backward_iterator backward_begin() const{
         return backward_iterator(root);
     }
-    backward_iterator bacward_end() const{
+    backward_iterator backward_end() const{
         return backward_iterator(nullptr);
     }
-    void insert(const value& element);
-    bool contains(const value& element) const;
-    void remove(const value& element);
     void print() const;
-    size_t get_size() const;
-    bool is_empty() const;
 };
 
 template<class value,class comparator>
-class binary_search_tree<value,comparator>::node** binary_search_tree<value,comparator>::find_min_node(node** root){
+class binary_search_tree<value,comparator>::node** binary_search_tree<value,comparator>::find_min(node** root){
     node** curr = root;
     while((*curr)->left)
-        curr = &(*curr)->left;
+        curr = &((*curr)->left);
     return curr;
 }
 
 template<class value,class comparator>
-class binary_search_tree<value,comparator>::node* binary_search_tree<value,comparator>::copy(node* root){
-    if(!root) return root;
-    node* copy = new node(root->val);
-    copy->left = copy(root->left);
-    copy->right = copy(root->right);
-    return copy;
-}
-
-template<class value,class comparator>
-void binary_search_tree<value,comparator>::move(const binary_search_tree& other){
-    this->root = other.root;
-    this->cmp = other.cmp;
-    this->size = other.size;
-    
-    other.root = nullptr;
-    other.size = 0;
-}
-
-template<class value,class comparator>
-void binary_search_tree<value,comparator>::destroy(node* root){
-    if(!root) return;
-    destroy(root->left);
-    destroy(root->right);
-    delete root;
-}
-
-template<class value,class comparator>
-binary_search_tree<value,comparator>::binary_search_tree(const binary_search_tree& other){
-    root = copy(root);
-    size = other.size;
-    cmp = other.cmp;
-}
-
-template<class value,class comparator>
-binary_search_tree<value,comparator>::binary_search_tree(binary_search_tree&& other){
-    move(std::move(other));
-}
-
-template<class value,class comparator>
-binary_search_tree<value,comparator>::binary_search_tree(const comparator& cmp){
-    this->cmp = cmp;
-}
-
-template<class value,class comparator>
-binary_search_tree<value,comparator>& binary_search_tree<value,comparator>::operator=(const binary_search_tree<value,comparator>& other){
-    if(*this!=other){
-        destroy(root);
-        root = copy(other.root);
-        size = other.size;
-        cmp = other.cmp;
+void binary_search_tree<value,comparator>::insert(const value& _value){
+    node** curr = &root;
+    std::stack<node*> to_increase;
+    while(*curr){
+        to_increase.push(*curr);
+        if(cmp((*curr)->val,_value))
+            curr = &((*curr)->right);
+        else if(cmp(_value,(*curr)->val))
+            curr = &((*curr)->left);
+        else return;
     }
-    return *this;
-}
-
-template<class value,class comparator>
-binary_search_tree<value,comparator>& binary_search_tree<value,comparator>::operator=( binary_search_tree<value,comparator>&& other){
-    if(*this!=other){
-        destroy(root);
-        move(std::move(other));
+    *curr = new node(_value);
+    while(!to_increase.empty()){
+        node* curr_top = to_increase.top();
+        curr_top->subtree_size++;
+        to_increase.pop();
     }
-    return *this;
 }
 
 template<class value,class comparator>
-binary_search_tree<value,comparator>::~binary_search_tree(){
-    destroy(root);
-}
-
-template<class value,class comparator>
-void binary_search_tree<value,comparator>::insert(const value& element){
-    node** current = &root;
-    while(*current){
-        if(cmp((*current)->val,element))
-           current = &(*current)->right;
-        else if(cmp(element,(*current)->val))
-            current = &(*current)->left;
-        else
-            return;;
-    }
-    *current = new node(element);
-    size++;
-}
-
-template<class value,class comparator>
-bool binary_search_tree<value,comparator>::contains(const value& element) const{
+bool binary_search_tree<value,comparator>::contains(const value& _value) const{
     node* curr = root;
     while(curr){
-        if(cmp(curr->val,element))
+        if(cmp(curr->val,_value))
             curr = curr->right;
-        else  if(cmp(element,curr->val))
-            curr = curr->right;
+        else if(cmp(_value,curr->val))
+            curr = curr->left;
         else
             return true;
     }
@@ -234,58 +195,125 @@ bool binary_search_tree<value,comparator>::contains(const value& element) const{
 }
 
 template<class value,class comparator>
-void binary_search_tree<value,comparator>::remove(const value& element){
+void binary_search_tree<value,comparator>::remove(const value& _value){
     node** curr = &root;
+    std::stack<node*> to_decrease;
     while(*curr){
-        if(cmp((*curr)->val,element))
-            curr = &(*curr)->right;
-        else if(cmp(element,(*curr)->val))
-            curr = &(*curr)->left;
+        if(cmp((*curr)->val,_value))
+            curr = &((*curr)->right);
+        else if(cmp(_value,(*curr)->val))
+            curr = &((*curr)->left);
         else
             break;
+        to_decrease.push(*curr);
     }
     if(!(*curr))
         return;
     node* to_delete = *curr;
-    if(!(*curr)->left && !(*curr)->right)
+    if(!((*curr)->left) && !((*curr)->right))
         *curr = nullptr;
-    else if(!(*curr)->right)
-        *curr = (*curr)->left;
-    else if(!(*curr)->left)
+    else if(!((*curr)->left))
         *curr = (*curr)->right;
+    else if(!((*curr)->right))
+        *curr = (*curr)->left;
     else{
-        node** right_min = find_min_node(&(*curr)->right);
+        node** right_min = find_min(&((*curr)->right));
         *curr = *right_min;
         *right_min = (*right_min)->right;
-        
         (*curr)->left = to_delete->left;
         (*curr)->right = to_delete->right;
     }
     delete to_delete;
-    size--;
+    while(!to_decrease.empty()){
+        node* curr_top = to_decrease.top();
+        to_decrease.pop();
+        curr_top->subtree_size--;
+    }
+}
+
+
+template<class value,class comparator>
+const value& binary_search_tree<value,comparator>::operator[](size_t index) const{
+    return get_element_at_index(this->root, index);
+}
+
+template<class value,class comparator>
+bool binary_search_tree<value,comparator>::is_empty() const{
+    return root == nullptr;
+}
+
+template<class value,class comparator>
+size_t binary_search_tree<value,comparator>::size() const{
+    return root != nullptr ? root->subtree_size : 0;
+}
+
+template<class value,class comparator>
+class binary_search_tree<value,comparator>::node* binary_search_tree<value,comparator>::copy(node* root){
+    if(!root)
+        return nullptr;
+    node* res = new node(root->val);
+    res->left = copy(root->left);
+    res->right = copy(root->right);
+    return res;
+}
+
+template<class value,class comparator>
+void binary_search_tree<value,comparator>::destory(node* root){
+    if(!root)
+        return;
+    destory(root->left);
+    destory(root->right);
+    delete root;
+}
+
+template<class value,class comparator>
+binary_search_tree<value,comparator>::~binary_search_tree(){
+    destory(this->root);
+}
+
+template<class value,class comparator>
+binary_search_tree<value,comparator>::binary_search_tree(const binary_search_tree& other){
+    copy(other.root);
+    this->cmp = other.cmp;
+}
+
+template<class value,class comparator>
+binary_search_tree<value,comparator>& binary_search_tree<value,comparator>::operator=(const binary_search_tree& other){
+    if(this!=&other){
+        destroy(root);
+        copy(other.root);
+        this->cmp = other.cmp;
+    }
+    return *this;
+}
+
+
+template<class value,class comparator>
+binary_search_tree<value,comparator>& binary_search_tree<value,comparator>::operator=( binary_search_tree&& other){
+    if(this!=&other){
+        destroy(root);
+        move(std::move(other));
+        this->cmp = other.cmp;
+    }
+    return *this;
+}
+
+
+template<class value,class comparator>
+void binary_search_tree<value,comparator>::move(binary_search_tree&& other){
+    this->root = other.root;
+    this->cmp = other.cmp;
+    other.root = nullptr;
+}
+
+
+template<class value,class comparator>
+binary_search_tree<value,comparator>::binary_search_tree(binary_search_tree&& other){
+    move(std::move(other));
 }
 
 
 template<class value,class comparator>
 void binary_search_tree<value,comparator>::print() const{
-    print_helper(root);
+    print_helper(this->root);
 }
-
-template<class value,class comparator>
-void binary_search_tree<value,comparator>::print_helper(node* root) const{
-    if(!root) return;
-    print_helper(root->left);
-    std::cout<<root->val<<" ";
-    print_helper(root->right);
-}
-
-template<class value,class comparator>
-bool binary_search_tree<value,comparator>::is_empty() const{
-    return size == 0;
-}
-
-template<class value,class comparator>
-size_t binary_search_tree<value,comparator>::get_size() const{
-    return size;
-}
-
--- Implementing Data Structures/binary_search_tree/main.cpp
+++ Implementing Data Structures/binary_search_tree/main.cpp
@@ -2,22 +2,23 @@
 
 int main(int argc, const char * argv[]) {
     binary_search_tree<int> bst;
-    bst.insert(1);
-    bst.insert(18);
-    bst.insert(0);
-    bst.insert(22);
+   
+    for(int i = 0;i<100;i++)
+        bst.insert(i);
     
-        
+    //bst.print();
+    std::cout<<bst[0]<<std::endl;
+    std::cout<<bst[bst.size() - 1]<<std::endl;
     
     auto it = bst.forward_begin();
     it++;
     it++;
-    it++;
+    std::cout<<*it<<std::endl;
     
     auto it2 = bst.backward_begin();
     it2--;
-    it2--;
-    it2--;
     std::cout<<*it2<<std::endl;
-
+    
+    std::cout<<bst.size()<<std::endl;
+    
 }
