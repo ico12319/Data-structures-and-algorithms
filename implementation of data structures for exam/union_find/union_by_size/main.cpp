1e358eaa1957578918034ffcc490c0297730e5e6
ico12319 <hristokubev@gmail.com>
2025-02-23 16:44:12 +0200

    reorganize

--- implementation of data structures for exam/union_find/union_by_size/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "union_find.h"
-
-int main(int argc, const char * argv[]) {
-    union_find uf(5);
-    uf.union_by_size(0, 1);
-    uf.union_by_size(1, 2);
-    
-    std::cout<<uf.connected_components_count()<<std::endl;
-}
